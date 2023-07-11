668cd498f0c1cd2a1c76f234dca240fed72910e1
Rhitik V Nimbalkar <74248352+nrhitik@users.noreply.github.com>
2023-07-11 19:12:28 +0530

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -27,3 +27,32 @@ def wishMe():
         speak("Good Evening!")
 
     speak("I am Steve Sir. Please tell me how may I help you.") # AI intro.
+
+def takeCommand():
+    '''It takes microphone input from the user and returns string output.'''
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try: 
+        print("Recogninzing...")
+        query = r.recognize_google(audio, language = 'en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e) # Will print exception/error.
+        print("Say that again please...")
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    while 1:
+        # Logic for executing tesks based on query.
+        query = takeCommand().lower()
+        if 'open spotify' in query:
+            codepath = "" #target of spotify
+            os.startfile(codepath)
